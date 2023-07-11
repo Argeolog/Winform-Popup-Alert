ede2659e0e1738f4b48bb9b8436168b0b1108838
Hasan Atmaca <104566717+Argeolog@users.noreply.github.com>
2023-07-11 13:18:40 +0300

    Add files via upload

--- VB.Net/Mesaj_Alert/Anasayfa.vb
+++ VB.Net/Mesaj_Alert/Anasayfa.vb
@@ -2,7 +2,7 @@
 	Sub Alert_Goster(ByVal Controller As Form, ByVal Txt As String)
 		Dim MesajAlert As New MesajAlertUser
 		MesajAlert.Set_Setting(Controller, Alert_Time_Out_Text.Value, Txt, Animasyon_Modu_Check.Checked)
-		Me.Controls.Add(MesajAlert)
+		Controller.Controls.Add(MesajAlert)
 		MesajAlert.BringToFront() ' Bunu Kontrol'ün içinde çağıramazdık. çünkü işlemiyor. Kulağı mecbur ters tuttuk. çünkü formun üstünde yıkıp yıkıp edebilirdi.
 
 	End Sub
